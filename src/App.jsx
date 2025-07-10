fe30a11253a65432e2dc6498716551d77fe97268
Satvika <162724268+satvikapadakanti@users.noreply.github.com>
2025-07-10 22:18:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import Header from "./components/Header.jsx";
-import Home from "./pages/Home.jsx";
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="min-h-screen">
-      <div className="bg-red-500 text-white p-8 text-center text-2xl m-4 rounded-lg">
-        🎂 TAILWIND TEST - Should be RED background with WHITE text
-      </div>
-      <Header />
-      <Home />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
